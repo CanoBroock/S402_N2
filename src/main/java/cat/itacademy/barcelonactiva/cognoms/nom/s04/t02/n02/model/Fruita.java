a25aa67e2fdb0b61daf8103d65509d110af55847
CanoBroock <canobroock@gmail.com>
2022-11-16 10:49:30 +0100

    correcion

---
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n02/model/Fruita.java
@@ -0,0 +1,60 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n02.model;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Frutitas")
+public class Fruita {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "Nombre_fruta")
+    private String nombre;
+
+    @Column(name = "Cantidad_kilos")
+    private int quantitatQuilos;
+
+    public Fruita() {
+    }
+
+    public Fruita(String nombre, int quantitatQuilos) {
+        this.id = id;
+        this.nombre = nombre;
+        this.quantitatQuilos = quantitatQuilos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getQuantitatQuilos() {
+        return quantitatQuilos;
+    }
+
+    public void setQuantitatQuilos(int quantitatQuilos) {
+        this.quantitatQuilos = quantitatQuilos;
+    }
+
+    @Override
+    public String toString() {
+        return "Fruita [id=" + id + ", nombre=" + nombre + ", quantitatQuilos=" + quantitatQuilos + "]";
+    }
+
+}
+
---
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n02/model/repository/FruitaRepository.java
@@ -0,0 +1,7 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n02.model.repository;
+
+import cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n02.model.Fruita;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FruitaRepository extends JpaRepository<Fruita, Integer> {
+}
